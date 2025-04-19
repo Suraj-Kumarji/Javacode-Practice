1ae11cf984d0a5a1ff8e499e728338656ad16250
unknown <sps887394sps@gmail.com>
2025-04-19 23:58:16 +0530

    Day5-3

---
+++ Day 5/GradeChecker.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class GradeChecker {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your age:");
+        int age=sc.nextInt();
+        
+        String category;
+        if(age<13){
+            category="Child";
+        }else if(age<20){
+            category="Teen";
+        }else if(age<60){
+            category="Adult";
+        }else{
+            category="Senior";
+        }
+
+        System.out.println("you belong to the age category:"+category);
+    }
+    
+}
