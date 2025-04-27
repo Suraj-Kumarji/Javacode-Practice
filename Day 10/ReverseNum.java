307c944d5ce798c836483d8f04e78e061fe36bb2
unknown <sps887394sps@gmail.com>
2025-04-27 22:58:35 +0530

    Day10-3

---
+++ Day 10/ReverseNum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReverseNum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number to be reversed:");
+        int num=sc.nextInt();
+        int reversed=0;
+        while(num!=0){
+            int digit=num%10;
+            reversed=reversed*10+digit;
+            num/=10;
+        }
+        System.out.println("Reversed number is:"+reversed);
+    }
+    
+}
